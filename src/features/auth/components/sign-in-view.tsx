a4b058dadf8f3cb1de13f25c2bad48f82904f560
le-dat <lqdat87@gmail.com>
2026-05-09 13:38:42 +0700

    refactor: remove GitHubStarsButton component and sign-in view integration

--- src/features/auth/components/sign-in-view.tsx
+++ src/features/auth/components/sign-in-view.tsx
@@ -58,14 +58,6 @@ export default function SignInViewPage() {
       </div>
       <div className='flex h-full items-center justify-center p-4 lg:p-8'>
         <div className='flex w-full max-w-md flex-col items-center justify-center space-y-6'>
-          {/* github link  */}
-          <GitHubStarsButton
-            owner='kiranism'
-            repo='next-shadcn-dashboard-starter'
-            showRepo
-            variant='outline'
-            size='default'
-          />
           <ClerkSignInForm
             initialValues={{
               emailAddress: 'your_mail+clerk_test@example.com'
